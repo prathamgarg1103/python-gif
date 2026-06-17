7f5d3316d82ab73ad759e73535c2ad564d121f52
prathamgarg1103 <pgarg7_be23@thapar.edu>
2026-06-17 11:07:17 +0530

    sasuke push

--- create_gif.py
+++ create_gif.py
@@ -1,9 +1,9 @@
 import imageio.v3 as iio
 
-filenames = ['team-pic1.png', 'team-pic2.png']
+filenames = ['Screenshot from 2025-07-11 17-42-55.png', 'Screenshot from 2025-07-11 17-43-04.png']
 images = [ ]
 
 for filename in filenames:
   images.append(iio.imread(filename))
 
-iio.imwrite('team.gif', images, duration = 200, loop = 0)
+iio.imwrite('sasuke.gif', images, duration = 200, loop = 0)
